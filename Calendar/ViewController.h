95e3de8b144dc7dc147a1d93882d574d59bc2c6e
Christopher Yang <cyang001@citymail.cuny.edu>
2014-07-21 16:49:51 -0400

    First commit

---
+++ Calendar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calendar
+//
+//  Created by Christopher Yang on 7/8/14.
+//  Copyright (c) 2014 mxmum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Calendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calendar
+//
+//  Created by Christopher Yang on 7/8/14.
+//  Copyright (c) 2014 mxmum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
