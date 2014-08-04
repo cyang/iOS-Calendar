9347b045fa12d5b238e2bafe7d6bc025e0f010c3
Christopher Yang <cyang001@citymail.cuny.edu>
2014-08-04 11:51:06 -0400

    Fix text view in authenticationviewcontroller

---
+++ Calendar/SearchBar.h
@@ -0,0 +1,13 @@
+//
+//  SearchBar.h
+//  mxmum
+//
+//  Created by Christopher Yang on 8/4/14.
+//  Copyright (c) 2014 mxmum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBar : UISearchBar
+
+@end
