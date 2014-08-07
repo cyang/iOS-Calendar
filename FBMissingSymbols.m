80027730a129732651bcbf684fc043b23f0ea6ab
Christopher Yang <cyang001@citymail.cuny.edu>
2014-08-06 23:58:32 -0400

    Parse login succes

---
+++ FBMissingSymbols.m
@@ -0,0 +1,35 @@
+//
+//  FBMissingSymbols.m
+//  mxmum
+//
+//  Created by Christopher Yang on 8/6/14.
+//  Copyright (c) 2014 mxmum. All rights reserved.
+//
+
+
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
+
+
